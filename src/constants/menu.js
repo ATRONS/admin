cac1adbb3ff9488269062771b7b6ba6a715be5fc
Henok Dejen <henokdejen84@gmail.com>
2020-11-13 15:19:58 +0100

    Added menus for materials and providers along with their respective submenus

--- src/constants/menu.js
+++ src/constants/menu.js
@@ -1,4 +1,4 @@
-import { adminRoot } from "./defaultValues";
+import { adminRoot } from './defaultValues';
 // import { UserRole } from "../helpers/authHelper"
 
 const data = [
@@ -16,19 +16,55 @@ const data = [
     ],
   },
   {
-    id: 'secondmenu',
+    id: 'resource-providers',
     icon: 'iconsminds-three-arrow-fork',
-    label: 'menu.second-menu',
-    to: `${adminRoot}/second-menu`,
+    label: 'menu.resource-providers',
+    to: `${adminRoot}/resource-providers`,
     // roles: [UserRole.Admin, UserRole.Editor],
     subs: [
       {
         icon: 'simple-icon-paper-plane',
-        label: 'menu.second',
-        to: `${adminRoot}/second-menu/second`,
+        label: 'menu.companies',
+        to: `${adminRoot}/resource-providers/companies`,
+      },
+      {
+        icon: 'simple-icon-paper-plane',
+        label: 'menu.authors',
+        to: `${adminRoot}/resource-providers/authors`,
       },
     ],
   },
+
+  {
+    id: 'menu.materials',
+    icon: 'iconsminds-three-arrow-fork',
+    label: 'menu.materials',
+    to: `${adminRoot}/materials`,
+    // roles: [UserRole.Admin, UserRole.Editor],
+    subs: [
+      {
+        icon: 'simple-icon-paper-plane',
+        label: 'menu.books',
+        to: `${adminRoot}/materials/books`,
+      },
+      {
+        icon: 'simple-icon-paper-plane',
+        label: 'menu.magazines',
+        to: `${adminRoot}/materials/magazines`,
+      },
+      {
+        icon: 'simple-icon-paper-plane',
+        label: 'menu.newspaper',
+        to: `${adminRoot}/materials/newspapers`,
+      },
+      {
+        icon: 'simple-icon-paper-plane',
+        label: 'menu.add-material',
+        to: `${adminRoot}/materials/add-material`,
+      },
+    ],
+  },
+
   {
     id: 'blankpage',
     icon: 'iconsminds-bucket',
