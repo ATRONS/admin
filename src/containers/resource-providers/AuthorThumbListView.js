d1b96ec56f9b502e26c94b5f1967f07ca7ac6ac2
Henok Dejen <henokdejen84@gmail.com>
2020-11-13 15:24:05 +0100

    Created thumb list view for authors and companies

---
+++ src/containers/resource-providers/AuthorThumbListView.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import {
+  Card,
+  CardBody,
+  CardSubtitle,
+  CardText,
+  CustomInput,
+} from 'reactstrap';
+import classnames from 'classnames';
+import { ContextMenuTrigger } from 'react-contextmenu';
+import { Colxx } from '../../components/common/CustomBootstrap';
+import ThumbnailImage from '../../components/cards/ThumbnailImage';
+
+const AuthorThumbListView = ({ author, isSelect, collect, onCheckItem }) => {
+  const authorProfileUrl = 'authors/' + author.id;
+  return (
+    <Colxx xxs="12" md="6" lg="4" key={author.id}>
+      <ContextMenuTrigger id="menu_id" data={author.id} collect={collect}>
+        <Card
+          onClick={(event) => onCheckItem(event, author.id)}
+          className={classnames('d-flex flex-row mb-4', {
+            active: isSelect,
+          })}
+        >
+          <NavLink to={authorProfileUrl} className="d-flex">
+            <ThumbnailImage
+              rounded
+              small
+              alt={author.title}
+              src={author.img}
+              className="m-4"
+            />
+          </NavLink>
+          <div className=" d-flex flex-grow-1 min-width-zero">
+            <CardBody className=" pl-0 align-self-center d-flex flex-column flex-lg-row justify-content-between min-width-zero">
+              <div className="min-width-zero">
+                <NavLink to={authorProfileUrl}>
+                  <CardSubtitle className="truncate mb-1">
+                    {author.legal_name}
+                  </CardSubtitle>
+                </NavLink>
+                <CardText className="text-muted text-small mb-2">
+                  {/* {data.status} */}
+                  27 books
+                </CardText>
+              </div>
+              <div className="custom-control custom-checkbox pl-1 align-self-center">
+                <CustomInput
+                  className="item-check mb-0"
+                  type="checkbox"
+                  id={`check_${author.id}`}
+                  checked={isSelect}
+                  onChange={() => {}}
+                  label=""
+                />
+              </div>
+            </CardBody>
+          </div>
+        </Card>{' '}
+      </ContextMenuTrigger>
+    </Colxx>
+  );
+};
+
+/* React.memo detail : https://reactjs.org/docs/react-api.html#reactpurecomponent  */
+export default React.memo(AuthorThumbListView);
---
+++ src/containers/resource-providers/CompanyListView.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Card, CustomInput, Badge } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+import classnames from 'classnames';
+import { ContextMenuTrigger } from 'react-contextmenu';
+import { Colxx } from '../../components/common/CustomBootstrap';
+
+const CompanyListView = ({ company, isSelect, collect, onCheckItem }) => {
+  const companyProfileUrl = 'companies/' + company.id;
+
+  return (
+    <Colxx xxs="12" key={company.id} className="mb-3">
+      <ContextMenuTrigger id="menu_id" data={company.id} collect={collect}>
+        <Card
+          onClick={(event) => onCheckItem(event, company.id)}
+          className={classnames('d-flex flex-row', {
+            active: isSelect,
+          })}
+        >
+          <NavLink to={companyProfileUrl} className="d-flex">
+            <img
+              alt={company.legal_name}
+              src={company.img}
+              className="list-thumbnail responsive border-0 card-img-left"
+            />
+          </NavLink>
+          <div className="pl-2 d-flex flex-grow-1 min-width-zero">
+            <div className="card-body align-self-center d-flex flex-column flex-lg-row justify-content-between min-width-zero align-items-lg-center">
+              <NavLink to={companyProfileUrl} className="w-40 w-sm-100">
+                <p className="list-item-heading mb-1 truncate">
+                  {company.legal_name}
+                </p>
+              </NavLink>
+              <p className="mb-1 text-muted text-small w-15 w-sm-100">
+                {/* {company.category} */}
+                Some category
+              </p>
+              <p className="mb-1 text-muted text-small w-15 w-sm-100">
+                {/* {company.date} */}
+                Lela tarik
+              </p>
+            </div>
+            <div className="custom-control custom-checkbox pl-1 align-self-center pr-4">
+              <CustomInput
+                className="item-check mb-0"
+                type="checkbox"
+                id={`check_${company.id}`}
+                checked={isSelect}
+                onChange={() => {}}
+                label=""
+              />
+            </div>
+          </div>
+        </Card>
+      </ContextMenuTrigger>
+    </Colxx>
+  );
+};
+
+/* React.memo detail : https://reactjs.org/docs/react-api.html#reactpurecomponent  */
+export default React.memo(CompanyListView);
