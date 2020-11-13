b932b20ce3ee418541dc4223c4b16464329ca62c
Henok Dejen <henokdejen84@gmail.com>
2020-11-13 15:22:58 +0100

    Created pages to list books, magazines, newspapers, and authors

---
+++ src/views/app/materials/Books.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Books = () => {
+  return <div>Some books</div>;
+};
+
+export default Books;
---
+++ src/views/app/resource-providers/company/companies.js
@@ -0,0 +1,269 @@
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
+
+import { servicePath } from '../../../../constants/defaultValues';
+
+import ResourceProvidersHeading from '../../../../containers/resource-providers/ResourceProvidersHeading';
+import AddNewModal from '../../../../containers/resource-providers/AddNewModal';
+import useMousetrap from '../../../../hooks/use-mousetrap';
+import { DeleteProviderModal } from '../../../../containers/resource-providers/DeleteProviderModal';
+import ProviderListing from '../../../../containers/resource-providers/ProviderListing';
+import AddNewCompany from '../../../../containers/resource-providers/AddNewCompany';
+
+const getIndex = (value, arr, prop) => {
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i][prop] === value) {
+      return i;
+    }
+  }
+  return -1;
+};
+
+const apiUrl = `${servicePath}/cakes/paging`;
+
+const orderOptions = [
+  { column: 'title', label: 'Author Name' },
+  { column: 'category', label: 'Category' },
+  { column: 'status', label: 'Status' },
+];
+const pageSizes = [4, 8, 12, 20];
+
+const categories = [
+  { label: 'Cakes', value: 'Cakes', key: 0 },
+  { label: 'Cupcakes', value: 'Cupcakes', key: 1 },
+  { label: 'Desserts', value: 'Desserts', key: 2 },
+];
+
+const companies_Dummy = [
+  {
+    id: 1,
+    legal_name: 'Alemayehu Gelagay',
+    display_name: 'Alazar Bizen',
+    email: 'alaazaaro@gmail.com',
+    is_company: true,
+    provides: 'BOOK',
+    img: 'http://localhost:3000/assets/img/products/bebinca-thumb.jpg',
+    company_info: {
+      hq_address: 'kazanchis',
+      founded_date: '12/11/1992',
+    },
+  },
+  {
+    id: 2,
+    legal_name: 'Alemayehu Gelagay',
+    display_name: 'Alazar Bizen',
+    email: 'alaazaaro@gmail.com',
+    is_company: true,
+    provides: 'BOOK',
+    img: 'http://localhost:3000/assets/img/products/bebinca-thumb.jpg',
+    company_info: {
+      hq_address: 'kazanchis',
+      founded_date: '12/11/1992',
+    },
+  },
+  {
+    id: 3,
+    legal_name: 'Alemayehu Gelagay',
+    display_name: 'Alazar Bizen',
+    email: 'alaazaaro@gmail.com',
+    is_company: true,
+    provides: 'BOOK',
+    img: 'http://localhost:3000/assets/img/products/bebinca-thumb.jpg',
+    company_info: {
+      hq_address: 'kazanchis',
+      founded_date: '12/11/1992',
+    },
+  },
+];
+
+const CompanyListPages = ({ match }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [displayMode, setDisplayMode] = useState('thumblist');
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedPageSize, setSelectedPageSize] = useState(8);
+  const [selectedOrderOption, setSelectedOrderOption] = useState({
+    column: 'title',
+    label: 'Product Name',
+  });
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [totalItemCount, setTotalItemCount] = useState(0);
+  const [totalPage, setTotalPage] = useState(1);
+  const [search, setSearch] = useState('');
+  const [selectedItems, setSelectedItems] = useState([]);
+  const [companies, setcompanies] = useState([]);
+  const [lastChecked, setLastChecked] = useState(null);
+
+  // delete author related
+  const [deleteModalOpen, setDeleteModalOpen] = useState(false);
+  const [] = useState(null);
+
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [selectedPageSize, selectedOrderOption]);
+
+  useEffect(() => {
+    // fetchData();
+    setTotalPage(1);
+    setcompanies(companies_Dummy);
+    setSelectedItems([]);
+    setTotalItemCount(companies_Dummy.length);
+    setIsLoaded(true);
+  }, [selectedPageSize, currentPage, selectedOrderOption, search]);
+
+  const onCheckItem = (event, id) => {
+    if (
+      event.target.tagName === 'A' ||
+      (event.target.parentElement && event.target.parentElement.tagName === 'A')
+    ) {
+      return true;
+    }
+    if (lastChecked === null) {
+      setLastChecked(id);
+    }
+
+    let selectedList = [...selectedItems];
+    if (selectedList.includes(id)) {
+      selectedList = selectedList.filter((x) => x !== id);
+    } else {
+      selectedList.push(id);
+    }
+    setSelectedItems(selectedList);
+
+    if (event.shiftKey) {
+      let newItems = [...companies];
+      const start = getIndex(id, newItems, 'id');
+      const end = getIndex(lastChecked, newItems, 'id');
+      newItems = newItems.slice(Math.min(start, end), Math.max(start, end) + 1);
+      selectedItems.push(
+        ...newItems.map((item) => {
+          return item.id;
+        })
+      );
+      selectedList = Array.from(new Set(selectedItems));
+      setSelectedItems(selectedList);
+    }
+    document.activeElement.blur();
+    return false;
+  };
+
+  const handleChangeSelectAll = (isToggle) => {
+    if (selectedItems.length >= companies.length) {
+      if (isToggle) {
+        setSelectedItems([]);
+      }
+    } else {
+      setSelectedItems(companies.map((x) => x.id));
+    }
+    document.activeElement.blur();
+    return false;
+  };
+
+  const onContextMenuClick = (e, data) => {
+    if (data.action == 'delete') {
+      setDeleteModalOpen(true);
+    }
+  };
+
+  const onContextMenu = (e, data) => {
+    const clickedProductId = data.data;
+    console.log(data);
+    // setauthorToDelete(data);
+    if (!selectedItems.includes(clickedProductId)) {
+      setSelectedItems([clickedProductId]);
+    }
+
+    return true;
+  };
+
+  useMousetrap(['ctrl+a', 'command+a'], () => {
+    handleChangeSelectAll(false);
+  });
+
+  useMousetrap(['ctrl+d', 'command+d'], () => {
+    setSelectedItems([]);
+    return false;
+  });
+
+  const handlecompanyDelete = () => {
+    console.log('deleting', selectedItems);
+  };
+
+  const addNewCompany = (payload) => {
+    console.log('Adding new company', payload);
+  };
+
+  const startIndex = (currentPage - 1) * selectedPageSize;
+  const endIndex = currentPage * selectedPageSize;
+
+  const deletablecompaniesName = companies
+    .filter((item) => selectedItems.includes(item.id))
+    .map((item) => item.legal_name);
+  console.log('s0', deletablecompaniesName);
+  return !isLoaded ? (
+    <div className="loading" />
+  ) : (
+    <>
+      <div className="disable-text-selection">
+        <ResourceProvidersHeading
+          heading="menu.companies"
+          displayMode={displayMode}
+          changeDisplayMode={setDisplayMode}
+          handleChangeSelectAll={handleChangeSelectAll}
+          changeOrderBy={(column) => {
+            setSelectedOrderOption(
+              orderOptions.find((x) => x.column === column)
+            );
+          }}
+          changePageSize={setSelectedPageSize}
+          selectedPageSize={selectedPageSize}
+          totalItemCount={totalItemCount}
+          selectedOrderOption={selectedOrderOption}
+          match={match}
+          startIndex={startIndex}
+          endIndex={endIndex}
+          selectedItemsLength={selectedItems ? selectedItems.length : 0}
+          itemsLength={companies ? companies.length : 0}
+          onSearchKey={(e) => {
+            if (e.key === 'Enter') {
+              setSearch(e.target.value.toLowerCase());
+            }
+          }}
+          orderOptions={orderOptions}
+          pageSizes={pageSizes}
+          toggleModal={() => setModalOpen(!modalOpen)}
+          handleDelteSelections={() => setDeleteModalOpen(true)}
+        />
+        <AddNewCompany
+          modalOpen={modalOpen}
+          toggleModal={() => setModalOpen(!modalOpen)}
+          categories={categories}
+          handleSubmit={addNewCompany}
+        />
+
+        <DeleteProviderModal
+          modalOpen={deleteModalOpen}
+          toggleModal={() => setDeleteModalOpen(!deleteModalOpen)}
+          onComfirm={handlecompanyDelete}
+          providersName={deletablecompaniesName}
+        />
+
+        <ProviderListing
+          providers={companies}
+          displayMode={displayMode}
+          selectedItems={selectedItems}
+          onCheckItem={onCheckItem}
+          currentPage={currentPage}
+          totalPage={totalPage}
+          onContextMenuClick={onContextMenuClick}
+          onContextMenu={onContextMenu}
+          onChangePage={setCurrentPage}
+          providerType="company"
+        />
+      </div>
+    </>
+  );
+};
+
+export default CompanyListPages;
