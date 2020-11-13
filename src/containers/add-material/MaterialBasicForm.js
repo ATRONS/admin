3e4599d79d25893718c8c4ff7bd3c2cb6702b9ae
Henok Dejen <henokdejen84@gmail.com>
2020-11-13 15:28:54 +0100

    created individual steps for adding new material

---
+++ src/containers/add-material/MaterialBasicForm.js
@@ -0,0 +1,82 @@
+/* eslint-disable no-param-reassign */
+import React, { useState } from 'react';
+import { FormGroup, Label, Row } from 'reactstrap';
+import { Formik, Form } from 'formik';
+import CustomSelectInput from '../../components/common/CustomSelectInput';
+import { Colxx } from '../../components/common/CustomBootstrap';
+import Select from 'react-select';
+import MaterialDropZone from './MaterialDropZone';
+import { injectIntl } from 'react-intl';
+import * as Yup from 'yup';
+import { FormikReactSelect } from '../resource-providers/FormikFields';
+
+const selectData = [
+  { label: 'Book', value: 'book', key: 0 },
+  { label: 'Newspaper', value: 'newspapaer', key: 1 },
+  { label: 'Magazine', value: 'magazine', key: 2 },
+];
+const validateType = (value) => {
+  let error;
+  if (!value) {
+    error = 'Please enter your name';
+  }
+  return error;
+};
+
+const MaterialBasicSchema = Yup.object().shape({
+  type: Yup.string().required('Please select one of the options'),
+  file: Yup.object().nullable().required('Material file is required'),
+});
+
+const MaterialBasicForm = ({ innerRef, intl }) => {
+  const [selectedOption, setSelectedOption] = useState('');
+  const [file, setFile] = useState(null);
+  const { messages } = intl;
+
+  return (
+    <div className="wizard-basic-step">
+      <Formik
+        innerRef={innerRef}
+        initialValues={{
+          type: '',
+          file: { some: 1 },
+        }}
+        onSubmit={() => {}}
+        validationSchema={MaterialBasicSchema}
+      >
+        {({ values, errors, touched, setFieldTouched, setFieldValue }) => (
+          <Form className="av-tooltip tooltip-label-right">
+            <FormGroup>
+              <Label>{messages['forms.material-type']}</Label>
+              <Row>
+                <Colxx xxs="12" xl="6">
+                  <FormikReactSelect
+                    name="type"
+                    value={values.type}
+                    options={selectData}
+                    onChange={setFieldValue}
+                    onBlur={setFieldTouched}
+                  />
+                </Colxx>
+              </Row>
+
+              {errors.type && touched.type && (
+                <div className="invalid-feedback d-block">{errors.type}</div>
+              )}
+            </FormGroup>
+
+            <FormGroup>
+              <Label>{messages['forms.select-file']}</Label>
+              <MaterialDropZone onChange={setFieldValue} />
+              {errors.file && touched.file && (
+                <div className="invalid-feedback d-block">{errors.file}</div>
+              )}
+            </FormGroup>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default injectIntl(MaterialBasicForm);
---
+++ src/containers/add-material/MaterialDetailedForm.js
@@ -0,0 +1,154 @@
+/* eslint-disable no-param-reassign */
+import React from 'react';
+import { FormGroup, Label } from 'reactstrap';
+import { Formik, Form, Field } from 'formik';
+import MaterialDropZone from './MaterialDropZone';
+import DatePicker from 'react-datepicker';
+import { FormikReactSelect } from '../resource-providers/FormikFields';
+import * as Yup from 'yup';
+import { injectIntl } from 'react-intl';
+
+const selectData = [
+  { label: 'Book', value: 'book', key: 0 },
+  { label: 'Newspaper', value: 'newspapaer', key: 1 },
+  { label: 'Magazine', value: 'magazine', key: 2 },
+];
+
+const MaterialDetailSchema = Yup.object().shape({
+  title: Yup.string().required('Please select one of the options'),
+  subtitle: Yup.string().notRequired().max(50, "Can't exceed 50 characters"),
+  cover_img_url: Yup.string().required('Cover image is required'),
+  published_date: Yup.date().nullable().required('Published date is required'),
+  display_date: Yup.date().notRequired().nullable(),
+  pages: Yup.number().required('Number of pages is required'),
+  edition: Yup.number().required('Edition number is required'),
+});
+
+const MaterialDetailedForm = ({ innerRef, intl }) => {
+  const { messages } = intl;
+
+  return (
+    <div className="wizard-basic-step">
+      <Formik
+        innerRef={innerRef}
+        initialValues={{
+          title: '',
+          subtitle: '',
+          cover_img_url: '',
+          published_date: null,
+          display_date: '',
+          ISBN: '',
+          synopsis: '',
+          review: '',
+          tags: [],
+          pages: '',
+          edition: '',
+        }}
+        onSubmit={() => {}}
+        validationSchema={MaterialDetailSchema}
+      >
+        {({ values, errors, touched, setFieldValue, setFieldTouched }) => (
+          <Form className="av-tooltip tooltip-label-right">
+            <FormGroup>
+              <Label>{messages['forms.title']}</Label>
+              <Field className="form-control" name="title" />
+              {errors.title && touched.title && (
+                <div className="invalid-feedback d-block">{errors.title}</div>
+              )}
+            </FormGroup>
+
+            <FormGroup>
+              <Label>{messages['forms.sub-title']}</Label>
+              <Field className="form-control" name="subtitle" />
+              {errors.subtitle && touched.subtitle && (
+                <div className="invalid-feedback d-block">
+                  {errors.subtitle}
+                </div>
+              )}
+            </FormGroup>
+
+            <FormGroup>
+              <Label>{messages['forms.cover-image']}</Label>
+              <MaterialDropZone />
+              {errors.cover_img_url && touched.cover_img_url && (
+                <div className="invalid-feedback d-block">
+                  {errors.cover_img_url}
+                </div>
+              )}
+            </FormGroup>
+
+            <FormGroup>
+              <Label>{messages['forms.published-date']}</Label>
+              <DatePicker
+                selected={values.published_date}
+                onChange={(d) => setFieldValue('published_date', d)}
+                placeholderText={messages['forms.date']}
+              />
+              {errors.published_date && touched.published_date && (
+                <div className="invalid-feedback d-block">
+                  {errors.published_date}
+                </div>
+              )}
+            </FormGroup>
+
+            <FormGroup>
+              <Label>{messages['forms.isbn']}</Label>
+              <Field className="form-control" name="ISBN" />
+              {errors.ISBN && touched.ISBN && (
+                <div className="invalid-feedback d-block">{errors.ISBN}</div>
+              )}
+            </FormGroup>
+            <FormGroup>
+              <Label>{messages['forms.synopsis']}</Label>
+              <Field
+                className="form-control"
+                name="synopsis"
+                component="textarea"
+              />
+
+              {errors.synopsis && touched.synopsis && (
+                <div className="invalid-feedback d-block">
+                  {errors.synopsis}
+                </div>
+              )}
+            </FormGroup>
+            <FormGroup>
+              <Label>{messages['forms.tags']}</Label>
+              <FormikReactSelect
+                name="tags"
+                value={values.tags}
+                options={selectData}
+                isMulti
+                onChange={setFieldValue}
+                onBlur={setFieldTouched}
+              />
+              {errors.tags && touched.tags && (
+                <div className="invalid-feedback d-block">{errors.tags}</div>
+              )}
+            </FormGroup>
+
+            <FormGroup>
+              <Label>{messages['forms.pages']}</Label>
+              <Field className="form-control" name="pages" type="number" />
+
+              {errors.pages && touched.pages && (
+                <div className="invalid-feedback d-block">{errors.pages}</div>
+              )}
+            </FormGroup>
+
+            <FormGroup>
+              <Label>{messages['forms.edition']}</Label>
+              <Field className="form-control" name="edition" type="number" />
+
+              {errors.edition && touched.edition && (
+                <div className="invalid-feedback d-block">{errors.edition}</div>
+              )}
+            </FormGroup>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default injectIntl(MaterialDetailedForm);
