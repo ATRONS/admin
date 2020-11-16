ff6d7d61e7e747d9cf6806597a5baed496acd133
Henok Dejen <henokdejen84@gmail.com>
2020-11-16 12:14:01 +0100

    Created dummy data for books

---
+++ src/data/book.js
@@ -0,0 +1,32 @@
+export const books = [
+  {
+    type: 'BOOK',
+    title: 'ሐሰተኛው',
+    subtitle: 'በእምነት ስም',
+    file: {
+      id: '5f9687779369b33188a24906',
+      size: 4004,
+      contentType: 'image/png',
+      mimetype: 'image/png',
+      url: '/media/materials/5f9687779369b33188a24906',
+    },
+    cover_img_url: '/media/images/someidhere',
+    published_date: '11/12/2013',
+    display_date: 'ግንቦት 2005',
+    ISBN: '5face2c3e90aba294074feed',
+    synopsis: 'REQUIRED FOR TYPE BOOK, NOT ALLOWED FOR OTHER TYPES',
+    review: 'OPTIONAL',
+    tags: ['5face2c3e90aba294074feed'],
+    pages: 302,
+    edition: 1,
+    provider: '5f9687779369b33188a24906',
+    price: {
+      free: false,
+      selling: 15,
+      rent: {
+        value: 5,
+        per: 1,
+      },
+    },
+  },
+];
