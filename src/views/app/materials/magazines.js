9ffa5a2c538b7d33d25973e977e3e6b97cfaf168
Henok Dejen <henokdejen84@gmail.com>
2020-11-16 12:14:55 +0100

    Created magazine lisiting page

--- src/views/app/materials/magazines.js
+++ src/views/app/materials/magazines.js
@@ -1,7 +1,113 @@
-import React from 'react';
+import Breadcrumb from '../../../containers/navs/Breadcrumb';
 
-const Magazines = () => {
-  return <div>Some Magazines</div>;
+import React, { useState } from 'react';
+import { Row } from 'reactstrap';
+import { Colxx, Separator } from '../../../components/common/CustomBootstrap';
+import { injectIntl } from 'react-intl';
+import { Card, CardBody } from 'reactstrap';
+
+import products from '../../../data/products';
+import { NavLink } from 'react-router-dom';
+import MyTable from '../../../containers/materials/common-table';
+
+const Magazines = ({ match }) => {
+  const [] = useState('');
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = React.useState(false);
+  const [pageCount, setPageCount] = React.useState(0);
+  const fetchIdRef = React.useRef(0);
+
+  const fetchData = React.useCallback(
+    ({ pageSize, pageIndex, searchKeyword = '' }) => {
+      console.log('Fetching');
+      const fetchId = ++fetchIdRef.current;
+      setLoading(true);
+      setTimeout(() => {
+        // Only update the data if this is the latest fetch
+        if (fetchId === fetchIdRef.current) {
+          const startRow = pageSize * pageIndex;
+          const endRow = startRow + pageSize;
+          let newProducts = products.filter((product) =>
+            product.title
+              .toLocaleLowerCase()
+              .includes(searchKeyword.toLocaleLowerCase())
+          );
+          setBooks(newProducts.slice(startRow, endRow));
+          setPageCount(Math.ceil(newProducts.length / pageSize));
+          setLoading(false);
+        }
+      }, 1000);
+    },
+    []
+  );
+
+  const cols = React.useMemo(
+    () => [
+      {
+        Header: 'Title',
+        accessor: 'title',
+        cellClass: 'list-item-heading ',
+        Cell: (props) => {
+          return (
+            <NavLink to={`magazines/${props.row.original.id}`}>
+              {props.value}
+            </NavLink>
+          );
+        },
+      },
+      {
+        Header: 'Company',
+        accessor: 'sales',
+        cellClass: 'text-muted ',
+        Cell: (props) => <>{props.value}</>,
+      },
+      {
+        Header: 'Edition',
+        accessor: 'category',
+        cellClass: 'text-muted ',
+        Cell: (props) => <>{props.value}</>,
+      },
+      {
+        Header: 'Published On',
+        accessor: 'createDate',
+        cellClass: 'text-muted  ',
+        Cell: (props) => <>{props.value}</>,
+      },
+      {
+        Header: 'Selling Price',
+        accessor: 'stock',
+        cellClass: 'text-muted ',
+        Cell: (props) => <>{props.value}</>,
+      },
+    ],
+    []
+  );
+
+  return (
+    <div className="materials-page-wrapper">
+      <Row>
+        <Colxx xxs="12">
+          <Breadcrumb heading="pages.magazines" match={match} />
+          <Separator className="mb-5" />
+        </Colxx>
+
+        <Colxx xxs="12">
+          <Card className="mb-4">
+            <CardBody>
+              <MyTable
+                columns={cols}
+                data={books}
+                pageCount={pageCount}
+                fetchData={fetchData}
+                loading={loading}
+                searchPlaceholder="pages.search-magazines"
+              />
+            </CardBody>
+          </Card>
+        </Colxx>
+      </Row>
+    </div>
+  );
 };
 
-export default Magazines;
+export default injectIntl(Magazines);
