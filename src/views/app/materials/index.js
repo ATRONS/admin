4974fdf90757f8d7410530d53b307e7ad133f8e5
Henok Dejen <henokdejen84@gmail.com>
2020-11-16 12:15:43 +0100

    added routes for book details and magazine listings

--- src/views/app/materials/index.js
+++ src/views/app/materials/index.js
@@ -1,8 +1,10 @@
 import React, { Suspense } from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
+import BookDetail from './BookDetail';
 // import { Books } from './Books';
 
 const Books = React.lazy(() => import('./Books'));
+const Magazines = React.lazy(() => import('./magazines'));
 const AddMaterialWizard = React.lazy(() => import('./addMaterial'));
 
 const Pages = ({ match }) => (
@@ -14,6 +16,18 @@ const Pages = ({ match }) => (
         exact
         render={(props) => <Books {...props} />}
       />
+      <Route
+        path={`${match.url}/books/:id`}
+        exact
+        render={(props) => <BookDetail {...props} />}
+      />
+
+      <Route
+        path={`${match.url}/magazines`}
+        exact
+        render={(props) => <Magazines {...props} />}
+      />
+
       <Route
         path={`${match.url}/add-material`}
         exact
