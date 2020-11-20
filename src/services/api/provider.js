ff4ab78ff7d82da0e9a1e67bf487048f1d6eef62
Henok Dejen <henokdejen84@gmail.com>
2020-11-20 16:44:44 +0100

    created api for providers

---
+++ src/services/api/provider.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+import { ApiCore } from './core';
+import { handleError, handleResponse } from './response';
+import URLs from './urls';
+
+const url = 'users/providers';
+const plural = 'users/providers';
+const single = 'users/provider';
+
+// plural and single may be used for message logic if needed in the ApiCore class.
+
+const apiProviders = new ApiCore({
+  getAll: true,
+  getSingle: true,
+  post: true,
+  put: false,
+  patch: true,
+  delete: false,
+  url: url,
+  plural: plural,
+  single: single,
+});
+
+apiProviders.books = (authorId, params) => {
+  // Add custom api call logic here
+  return axios
+    .get(`${URLs.BASE_URL}/${single}/${authorId}/books`, { params })
+    .then(handleResponse)
+    .catch(handleError);
+};
+
+apiProviders.deactivate = (authorId, params) => {
+  // Add custom api call logic here
+  return axios
+    .get(`${URLs.BASE_URL}/${single}/${authorId}/deactivate`, { params })
+    .then(handleResponse)
+    .catch(handleError);
+};
+
+export default apiProviders;
