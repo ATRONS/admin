7169f9c66350b59f52911b1571b34296f8063f5e
Henok Dejen <henokdejen84@gmail.com>
2020-11-20 16:45:13 +0100

    created an api for core

---
+++ src/services/api/core.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+import { apiProvider } from './main';
+import URLs from './urls';
+
+export const configureAxios = () => {
+  axios.defaults.baseURL = URLs.baseUrl;
+  // axios.defaults.headers.common['Authorization'] = ;
+  axios.defaults.headers.post['Content-Type'] = 'application/json';
+  window.axiosInstance = axios;
+};
+
+export class ApiCore {
+  constructor(options) {
+    if (options.getAll) {
+      this.getAll = (params) => {
+        return apiProvider.getAll(options.url, params);
+      };
+    }
+
+    if (options.getSingle) {
+      this.getSingle = (id) => {
+        return apiProvider.getSingle(options.url, id);
+      };
+    }
+
+    if (options.post) {
+      this.post = (model) => {
+        return apiProvider.post(options.url, model);
+      };
+    }
+
+    if (options.put) {
+      this.put = (model) => {
+        return apiProvider.put(options.url, model);
+      };
+    }
+
+    if (options.patch) {
+      this.patch = (model) => {
+        return apiProvider.patch(options.url, model);
+      };
+    }
+
+    if (options.remove) {
+      this.remove = (id) => {
+        return apiProvider.remove(options.url, id);
+      };
+    }
+  }
+}
