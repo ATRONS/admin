97ef0be709aec9c09ac1772315837829fd27addc
Henok Dejen <henokdejen84@gmail.com>
2020-11-21 00:25:37 +0100

    added the graph design configuration code

--- src/containers/common/SalesChartCard.js
+++ src/containers/common/SalesChartCard.js
@@ -9,12 +9,33 @@ import {
   DropdownMenu,
 } from 'reactstrap';
 
+import { ThemeColors } from '../../helpers/ThemeColors';
+
 import IntlMessages from '../../helpers/IntlMessages';
 import { LineChart } from '../../components/charts';
 
-import { lineChartData } from '../../data/charts';
+const colors = ThemeColors();
 
-const SalesChartCard = () => {
+const SalesChartCard = ({ data }) => {
+  const dataSet = {
+    labels: data.labels,
+    datasets: [
+      {
+        label: '',
+        data: data.values,
+        borderColor: colors.themeColor1,
+        pointBackgroundColor: colors.foregroundColor,
+        pointBorderColor: colors.themeColor1,
+        pointHoverBackgroundColor: colors.themeColor1,
+        pointHoverBorderColor: colors.foregroundColor,
+        pointRadius: 6,
+        pointBorderWidth: 2,
+        pointHoverRadius: 8,
+        fill: false,
+      },
+    ],
+  };
+
   return (
     <Card>
       <div className="position-absolute card-top-buttons">
@@ -40,7 +61,7 @@ const SalesChartCard = () => {
           <IntlMessages id="dashbaord.last_7_sells" />
         </CardTitle>
         <div className="dashboard-line-chart">
-          <LineChart shadow data={lineChartData} />
+          <LineChart shadow data={dataSet} />
         </div>
       </CardBody>
     </Card>
