6b0b3860c1392b801993155d456fdb5222515877
Henok Dejen <henokdejen84@gmail.com>
2020-11-21 00:29:52 +0100

    added helper functions to organize data from the server to graph input

--- src/containers/resource-providers/ProviderReporting.js
+++ src/containers/resource-providers/ProviderReporting.js
@@ -4,13 +4,47 @@ import { Colxx } from '../../components/common/CustomBootstrap';
 import IconCardsCarousel from '../common/IconCardsCarousel';
 import SalesChartCard from '../common/SalesChartCard';
 
-const ProviderReporting = () => {
+const extractSellsData = (data) => {
+  const labels = Array(data.length);
+  const values = Array(data.length);
+
+  let currentRow;
+  for (let i = 0; i < data.length; i++) {
+    currentRow = data[i];
+    labels[i] = `${currentRow._id.day} - ${currentRow._id.month}`;
+    values[i] = currentRow.total_amount;
+  }
+  console.log(values);
+  return { labels, values };
+};
+
+const parseSummaryReports = (data) => {
+  return [
+    {
+      title: 'dashbaord.available_amount',
+      icon: 'iconsminds-basket-coins',
+      value: 'ETB ' + data.available_balance,
+    },
+    {
+      title: 'dashbaord.total_amount',
+      icon: 'iconsminds-arrow-refresh',
+      value: 'ETB 74' + data.total_sells_amount,
+    },
+    {
+      title: 'dashbaord.total_books',
+      icon: 'iconsminds-mail-read',
+      value: 'ETB ' + data.total_materials_count,
+    },
+  ];
+};
+
+const ProviderReporting = ({ data }) => {
   return (
     <>
-      <IconCardsCarousel />
+      <IconCardsCarousel data={parseSummaryReports(data.summary)} />
       <Row>
         <Colxx md="12" className="mb-4">
-          <SalesChartCard />
+          <SalesChartCard data={extractSellsData(data.sells_report)} />
         </Colxx>
       </Row>
     </>
