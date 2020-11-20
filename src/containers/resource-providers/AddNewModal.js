9cdd0a37104fa9b018da0bdad49076b9db3ecb2a
Henok Dejen <henokdejen84@gmail.com>
2020-11-20 17:02:29 +0100

    added phone, about fileds to add author modal

--- src/containers/resource-providers/AddNewModal.js
+++ src/containers/resource-providers/AddNewModal.js
@@ -15,6 +15,9 @@ import {
 } from 'reactstrap';
 import { FormikDatePicker } from '../../containers/resource-providers/FormikFields';
 import IntlMessages from '../../helpers/IntlMessages';
+import apiProviders from '../../services/api/provider';
+
+const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
 
 const SignupSchema = Yup.object().shape({
   firstName: Yup.string()
@@ -28,30 +31,54 @@ const SignupSchema = Yup.object().shape({
   email: Yup.string()
     .email('Invalid email')
     .required('Please enter your email address'),
+  about: Yup.string()
+    .min(10, 'Too Short!')
+    .max(1000, 'Too Long!')
+    .required('Please enter your last name'),
+  phone: Yup.string().matches(phoneRegExp, 'Phone number is not valid'),
   dob: Yup.date().nullable().required('Date of birth required'),
   activeFrom: Yup.date().nullable().required('Date required'),
 });
 
 const AddNewAuthor = ({ modalOpen, toggleModal, handleSubmit }) => {
-  const onSubmit = (values) => {
+  const onSubmit = (values, actions) => {
     console.log(values);
 
-    const { firstName, lastName, displayName, email, dob, activeFrom } = values;
+    const {
+      firstName,
+      lastName,
+      displayName,
+      email,
+      dob,
+      activeFrom,
+      phone,
+      about,
+    } = values;
 
     const payload = {
       legal_name: `${firstName} ${lastName}`,
       display_name: displayName,
       email: email,
+      phone,
+      about,
       is_company: false,
       author_info: {
-        dob: dob,
-        active_from: activeFrom,
+        dob: dob.toISOString(),
+        active_from: activeFrom.toISOString(),
       },
+      password: 'Dummy',
       provides: 'BOOK',
     };
 
+    apiProviders.post(payload).then((res) => {
+      console.log('ache', res);
+      if (res.success) {
+        handleSubmit(res.data);
+      }
+      actions.setSubmitting(false);
+    });
+
     console.log('Sending', payload);
-    handleSubmit(payload);
   };
 
   return (
@@ -67,17 +94,27 @@ const AddNewAuthor = ({ modalOpen, toggleModal, handleSubmit }) => {
       <ModalBody>
         <Formik
           initialValues={{
-            firstName: '',
-            lastName: '',
-            displayName: '',
-            email: '',
-            dob: null,
-            activeFrom: null,
+            firstName: 'Henok',
+            lastName: 'Dejen',
+            displayName: 'Mukera',
+            email: 'henokdejen84@gmail.com',
+            dob: new Date(),
+            phone: '0911932901',
+            about:
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur diam ligula, ornare et mi sit amet, lacinia aliquam metus. Suspendisse ornare velit et velit imperdiet vehicula. Sed accumsan congue consectetur. Pellentesque dictum molestie eros, sed egestas nisl feugiat in. Proin fermentum, dolor id rutrum sollicitudin,',
+            activeFrom: new Date(),
           }}
           validationSchema={SignupSchema}
           onSubmit={onSubmit}
         >
-          {({ setFieldValue, setFieldTouched, values, errors, touched }) => (
+          {({
+            setFieldValue,
+            setFieldTouched,
+            values,
+            errors,
+            touched,
+            isSubmitting,
+          }) => (
             <Form className="av-tooltip tooltip-label-right">
               <FormGroup className="error-l-75">
                 <Label>First Name</Label>
@@ -110,6 +147,14 @@ const AddNewAuthor = ({ modalOpen, toggleModal, handleSubmit }) => {
                 ) : null}
               </FormGroup>
 
+              <FormGroup>
+                <Label>Phone</Label>
+                <Field className="form-control" name="phone" type="tel" />
+                {errors.phone && touched.phone ? (
+                  <div className="invalid-feedback d-block">{errors.phone}</div>
+                ) : null}
+              </FormGroup>
+
               <FormGroup className="error-l-100">
                 <Label className="d-block">Date of birth</Label>
                 <FormikDatePicker
@@ -138,12 +183,38 @@ const AddNewAuthor = ({ modalOpen, toggleModal, handleSubmit }) => {
                 ) : null}
               </FormGroup>
 
+              <FormGroup>
+                <Label>About the author</Label>
+                <Field
+                  className="form-control"
+                  name="about"
+                  component="textarea"
+                />
+                {errors.about && touched.about ? (
+                  <div className="invalid-feedback d-block">{errors.about}</div>
+                ) : null}
+              </FormGroup>
+
               <ModalFooter>
                 <Button color="secondary" outline onClick={toggleModal}>
                   <IntlMessages id="pages.cancel" />
                 </Button>
-                <Button color="primary" type="submit">
-                  <IntlMessages id="pages.submit" />
+
+                <Button
+                  color="primary"
+                  type="submit"
+                  className={`btn-shadow btn-multiple-state ${
+                    isSubmitting ? 'show-spinner' : ''
+                  }`}
+                >
+                  <span className="spinner d-inline-block">
+                    <span className="bounce1" />
+                    <span className="bounce2" />
+                    <span className="bounce3" />
+                  </span>
+                  <span className="label">
+                    <IntlMessages id="pages.submit" />
+                  </span>
                 </Button>
               </ModalFooter>
             </Form>
