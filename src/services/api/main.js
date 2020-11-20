e1c88164bfe9932e0fd3b6c34f905e62cf11a043
Henok Dejen <henokdejen84@gmail.com>
2020-11-20 16:50:06 +0100

    created a file for defining basic REST api methods

---
+++ src/services/api/main.js
@@ -0,0 +1,66 @@
+import axios from 'axios';
+import { handleError, handleResponse } from './response';
+// const URLs.BASE_URL = 'http://127.0.0.1:3333/api/v1';
+import URLs from './urls';
+
+/** @param {string} resource */
+const getAll = (resource, params) => {
+  return axios
+    .get(`${URLs.BASE_URL}/${resource}`, { params })
+    .then(handleResponse)
+    .catch(handleError);
+};
+
+/** @param {string} resource */
+/** @param {string} id */
+const getSingle = (resource, id) => {
+  return axios
+    .get(`${URLs.BASE_URL}/${resource}/${id}`)
+    .then(handleResponse)
+    .catch(handleError);
+};
+
+/** @param {string} resource */
+/** @param {object} model */
+const post = (resource, model) => {
+  return axios
+    .post(`${URLs.BASE_URL}/${resource}`, model)
+    .then(handleResponse)
+    .catch(handleError);
+};
+
+/** @param {string} resource */
+/** @param {object} model */
+const put = (resource, model) => {
+  return axios
+    .put(`${URLs.BASE_URL}/${resource}`, model)
+    .then(handleResponse)
+    .catch(handleError);
+};
+
+/** @param {string} resource */
+/** @param {object} model */
+const patch = (resource, model) => {
+  return axios
+    .patch(`${URLs.BASE_URL}/${resource}`, model)
+    .then(handleResponse)
+    .catch(handleError);
+};
+
+/** @param {string} resource */
+/** @param {string} id */
+const remove = (resource, id) => {
+  return axios
+    .delete(`${URLs.BASE_URL}/${resource}`, id)
+    .then(handleResponse)
+    .catch(handleError);
+};
+
+export const apiProvider = {
+  getAll,
+  getSingle,
+  post,
+  put,
+  patch,
+  remove,
+};
