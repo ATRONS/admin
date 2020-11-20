102163669389dc6b93ff025a0a819d9d886f6832
Henok Dejen <henokdejen84@gmail.com>
2020-11-20 16:42:10 +0100

    created dummy data for author, books and companies

---
+++ src/data/authors.js
@@ -0,0 +1,50 @@
+export const Authors_Dummy = [
+  {
+    id: 1,
+    legal_name: `henok dEjen`,
+    display_name: 'mukera',
+    email: 'henokdejen84@gmail.com',
+    phone: '+21911932901',
+    total_materials: '27 books',
+    bio:
+      'I’m a web developer. I spend my whole day, practically every day,experimenting with HTML, CSS, and JavaScript; dabbling with Python and Ruby; and inhaling a wide variety of potentially useless information through a few hundred RSS feeds. I build websites that delight and inform. I do it well.',
+    is_company: false,
+    img: 'http://localhost:3000/assets/img/products/bebinca-thumb.jpg',
+    author_info: {
+      dob: '11/09/1965',
+      active_from: '11/04/2000',
+    },
+  },
+  {
+    id: 2,
+    legal_name: `henok dEjen`,
+    display_name: 'mukera',
+    email: 'henokdejen84@gmail.com',
+    phone: '+21911932901',
+    total_materials: '14 books',
+    bio:
+      'I’m a web developer. I spend my whole day, practically every day,experimenting with HTML, CSS, and JavaScript; dabbling with Python and Ruby; and inhaling a wide variety of potentially useless information through a few hundred RSS feeds. I build websites that delight and inform. I do it well.',
+    is_company: false,
+    img: 'http://localhost:3000/assets/img/products/bebinca-thumb.jpg',
+    author_info: {
+      dob: '11/09/1965',
+      active_from: '11/04/2000',
+    },
+  },
+  {
+    id: 3,
+    legal_name: `henok dEjen`,
+    display_name: 'mukera',
+    email: 'henokdejen84@gmail.com',
+    phone: '+21911932901',
+    total_materials: '4 books',
+    bio:
+      'I’m a web developer. I spend my whole day, practically every day,experimenting with HTML, CSS, and JavaScript; dabbling with Python and Ruby; and inhaling a wide variety of potentially useless information through a few hundred RSS feeds. I build websites that delight and inform. I do it well.',
+    is_company: false,
+    img: '/assets/img/products/bebinca-thumb.jpg',
+    author_info: {
+      dob: '11/09/1965',
+      active_from: '11/04/2000',
+    },
+  },
+];
---
+++ src/data/companies.js
@@ -0,0 +1,50 @@
+export const companies_Dummy = [
+  {
+    id: 1,
+    legal_name: 'Alemayehu Gelagay',
+    display_name: 'Alazar Bizen',
+    email: 'alaazaaro@gmail.com',
+    is_company: true,
+    provides: 'BOOK',
+    phone: '+21911932901',
+    about:
+      'I’m a web developer. I spend my whole day, practically every day, experimenting with HTML, CSS, and JavaScript; dabbling with Python and Ruby; and inhaling a wide variety of potentially useless information through a few hundred RSS feeds. I build websites that delight and inform. I do it well.',
+    img: 'http://localhost:3000/assets/img/products/bebinca-thumb.jpg',
+    company_info: {
+      hq_address: 'kazanchis',
+      founded_date: '12/11/1992',
+    },
+  },
+  {
+    id: 2,
+    legal_name: 'Alemayehu Gelagay',
+    display_name: 'Alazar Bizen',
+    email: 'alaazaaro@gmail.com',
+    is_company: true,
+    provides: 'BOOK',
+    phone: '+21911932901',
+    about:
+      'I’m a web developer. I spend my whole day, practically every day, experimenting with HTML, CSS, and JavaScript; dabbling with Python and Ruby; and inhaling a wide variety of potentially useless information through a few hundred RSS feeds. I build websites that delight and inform. I do it well.',
+    img: 'http://localhost:3000/assets/img/products/bebinca-thumb.jpg',
+    company_info: {
+      hq_address: 'kazanchis',
+      founded_date: '12/11/1992',
+    },
+  },
+  {
+    id: 3,
+    legal_name: 'Alemayehu Gelagay',
+    display_name: 'Alazar Bizen',
+    email: 'alaazaaro@gmail.com',
+    is_company: true,
+    provides: 'BOOK',
+    phone: '+21911932901',
+    about:
+      'I’m a web developer. I spend my whole day, practically every day, experimenting with HTML, CSS, and JavaScript; dabbling with Python and Ruby; and inhaling a wide variety of potentially useless information through a few hundred RSS feeds. I build websites that delight and inform. I do it well.',
+    img: 'http://localhost:3000/assets/img/products/bebinca-thumb.jpg',
+    company_info: {
+      hq_address: 'kazanchis',
+      founded_date: '12/11/1992',
+    },
+  },
+];
