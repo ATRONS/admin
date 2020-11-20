f9d3119783e38d156fd51e47f23ec645a60d8598
Henok Dejen <henokdejen84@gmail.com>
2020-11-21 00:29:12 +0100

    added the main_url field

--- src/services/api/urls.js
+++ src/services/api/urls.js
@@ -1,5 +1,6 @@
-const BASE_URL = 'http://192.168.0.27:5000/api/v1/admin';
+const BASE_URL = 'http://10.6.196.165:5000/api/v1/admin';
+const MAIN_URL = 'http://10.6.196.165:5000/api/v1';
 const books = 'books/';
-const uploadMaterial = BASE_URL + '/upload/material'
+const uploadMaterial = BASE_URL + '/upload/material';
 
-export default { BASE_URL, books, uploadMaterial };
+export default { BASE_URL, MAIN_URL, books, uploadMaterial };
