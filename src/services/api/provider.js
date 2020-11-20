36f99e93c1e83287a4c403e07100cea4de1c577f
Henok Dejen <henokdejen84@gmail.com>
2020-11-21 00:26:53 +0100

    added searach author api endpoint

--- src/services/api/provider.js
+++ src/services/api/provider.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { ApiCore } from './core';
+import { apiProvider } from './main';
 import { handleError, handleResponse } from './response';
 import URLs from './urls';
 
@@ -25,6 +26,13 @@ apiProviders.books = (authorId, params) => {
   // Add custom api call logic here
   return axios
     .get(`${URLs.BASE_URL}/${single}/${authorId}/books`, { params })
+    .then(handleResponse)
+    .catch(handleError);
+};
+
+apiProviders.searchAuthor = (params) => {
+  return axios
+    .get(`${URLs.BASE_URL}/${url}`, { params })
     .then(handleResponse)
     .catch(handleError);
 };
