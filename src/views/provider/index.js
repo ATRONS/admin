7350e82c0c4234313a756a1af7448413a56183d5
Henok Dejen <henokdejen84@gmail.com>
2020-11-28 09:36:14 +0300

    add: added all the routes for the provider

--- src/views/provider/index.js
+++ src/views/provider/index.js
@@ -2,17 +2,59 @@ import React, { Suspense } from 'react';
 import { Route, withRouter, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import AppLayout from '../../layout/AppLayout';
+import ProviderLayout from '../../layout/ProviderLayout';
+const Materials = React.lazy(() =>
+  import(/* webpackChunkName: "viwes-blank-page" */ './materials')
+);
+
+const Reports = React.lazy(() =>
+  import(/* webpackChunkName: "viwes-blank-page" */ './reports')
+);
+
+const GetPaid = React.lazy(() =>
+  import(/* webpackChunkName: "viwes-blank-page" */ './payments/GetPaid')
+);
+
+const Requests = React.lazy(() =>
+  import(/* webpackChunkName: "viwes-blank-page" */ './requests')
+);
 
 const App = ({ match }) => {
   return (
-    <AppLayout>
+    <ProviderLayout>
       <div className="dashboard-wrapper">
         <Suspense fallback={<div className="loading" />}>
-          <h1>Henok</h1>
+          <Switch>
+            {/* <Redirect
+              exact
+              from={`${match.url}/`}
+              to={`${match.url}/materials`}
+            /> */}
+            <Route
+              path={`${match.url}/materials`}
+              render={(props) => <Materials {...props} />}
+            />
+
+            <Route
+              path={`${match.url}/reports`}
+              render={(props) => <Reports {...props} />}
+            />
+
+            <Route
+              path={`${match.url}/requests`}
+              render={(props) => <Requests {...props} />}
+            />
+
+            <Route
+              path={`${match.url}/payment/disburse`}
+              render={(props) => <GetPaid {...props} />}
+            />
+
+            <Redirect to="/error" />
+          </Switch>
         </Suspense>
       </div>
-    </AppLayout>
+    </ProviderLayout>
   );
 };
 
