7d7f0f9b12c80bb3c2a8e331a3d7d72e4d396908
Henok Dejen <henokdejen84@gmail.com>
2020-12-01 20:10:49 +0300

    feat: request list item

---
+++ src/components/applications/ProviderRequestListItem.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Card, CardBody, Badge, CustomInput } from 'reactstrap';
+import { NavLink } from 'react-router-dom';
+
+import { Colxx } from '../common/CustomBootstrap';
+
+const requestCategories = [
+  {
+    id: '0',
+    label: 'Pending',
+  },
+];
+
+const ProviderRequestListItem = ({ item }) => {
+  return (
+    <Colxx xxs="12">
+      <Card className="card d-flex mb-3">
+        <div className="d-flex flex-grow-1 min-width-zero">
+          <CardBody className="align-self-center d-flex flex-column flex-md-row justify-content-between min-width-zero align-items-md-center">
+            <span className="align-middle d-inline-block flex-grow-1">
+              {item.title}
+            </span>
+            <p className="mb-1 text-muted text-small w-xs-100 ml-4">
+              {item.category}
+            </p>
+            <p className="mb-1 text-muted text-small w-xs-100 ml-4">
+              {item.createDate.toLocaleDateString()}
+            </p>
+            <div className="mb-1 text-muted text-small w-xs-100 ml-4">
+              <Badge color={item.labelColor} pill>
+                {item.status}
+              </Badge>
+            </div>
+          </CardBody>
+        </div>
+        <div className="card-body pt-1">
+          <p className="mb-0">{item.describtion}</p>
+        </div>
+      </Card>
+    </Colxx>
+  );
+};
+
+export default React.memo(ProviderRequestListItem);
