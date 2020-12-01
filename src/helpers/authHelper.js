04bf66b5c28efe36555d2e12b37b23240a5da08a
Henok Dejen <henokdejen84@gmail.com>
2020-12-01 20:12:14 +0300

    changed the currentUserValue to be accessed from the props

--- src/helpers/authHelper.js
+++ src/helpers/authHelper.js
@@ -1,53 +1,55 @@
-
 import React from 'react';
-import {
-    Route,
-    Redirect,
-} from 'react-router-dom';
-import { getCurrentUser } from './Utils'
+import { Route, Redirect } from 'react-router-dom';
+import { getCurrentUser } from './Utils';
 import { isAuthGuardActive } from '../constants/defaultValues';
 
-const ProtectedRoute = ({ component: Component, roles = undefined, ...rest }) => {
-    const setComponent = (props) => {
-        if (isAuthGuardActive) {
-            const currentUser = getCurrentUser();
-            if (currentUser) {
-                if (roles) {
-                    if (roles.includes(currentUser.role)) {
-                        return <Component {...props} />;
-                    } else {
-                        return <Redirect
-                            to={{
-                                pathname: '/unauthorized',
-                                state: { from: props.location },
-                            }} />
-                    }
-                } else {
-                    return <Component {...props} />;
-                }
-            } else {
-                return <Redirect
-                    to={{
-                        pathname: '/user/login',
-                        state: { from: props.location },
-                    }} />
-            }
-        } else {
+const ProtectedRoute = ({
+  component: Component,
+  roles = undefined,
+  currentUser,
+  ...rest
+}) => {
+  const setComponent = (props) => {
+    if (isAuthGuardActive) {
+      console.log('lela', currentUser);
+      if (currentUser) {
+        if (roles) {
+          if (roles.includes(currentUser.role)) {
             return <Component {...props} />;
+          } else {
+            return (
+              <Redirect
+                to={{
+                  pathname: '/unauthorized',
+                  state: { from: props.location },
+                }}
+              />
+            );
+          }
+        } else {
+          return <Component {...props} />;
         }
-
+      } else {
+        return (
+          <Redirect
+            to={{
+              pathname: '/user/login',
+              state: { from: props.location },
+            }}
+          />
+        );
+      }
+    } else {
+      return <Component {...props} />;
     }
+  };
 
-    return (
-        <Route
-            {...rest}
-            render={setComponent}
-        />
-    );
-}
+  return <Route {...rest} render={setComponent} />;
+};
 const UserRole = {
-    Admin: 0,
-    Editor: 1,
-}
+  Admin: 'admin',
+  Editor: 1,
+  PROVIDER: 'provider',
+};
 
 export { ProtectedRoute, UserRole };
