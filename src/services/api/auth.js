db3d577b23f38f457a6c81012706bd2a0bf23231
Henok Dejen <henokdejen84@gmail.com>
2020-12-01 20:20:44 +0300

    added logout and changed the login path to the correct api end point

--- src/services/api/auth.js
+++ src/services/api/auth.js
@@ -2,21 +2,29 @@ import axios from 'axios';
 import { handleError, handleResponse } from './response';
 import URLs from './urls';
 
-const login = (username, password) => {
+const login = (email, password) => {
   return axios
-    .post(`${URLs.BASE_URL}/login`, { username, password })
+    .post(`${URLs.MAIN_URL}/account/login`, { email, password })
+    .then(handleResponse)
+    .catch(handleError);
+};
+
+const logout = () => {
+  return axios
+    .post(`${URLs.MAIN_URL}/account/logout`)
     .then(handleResponse)
     .catch(handleError);
 };
 
 const forgetPassword = (email) => {
   return axios
-    .post(`${URLs.BASE_URL}/forgetPassword`, { email })
+    .post(`${URLs.MAIN_URL}/forgetPassword`, { email })
     .then(handleResponse)
     .catch(handleError);
 };
 
 export const apiAuth = {
   login,
+  logout,
   forgetPassword,
 };
