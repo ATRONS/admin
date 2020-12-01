8c95ee74ebc27ddb49512cbe6808772c19ad4e32
Henok Dejen <henokdejen84@gmail.com>
2020-12-01 20:15:56 +0300

    created material related endpoints for providers

---
+++ src/services/api/provider-related/materials.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { ApiCore } from '../core';
+import { handleError, handleResponse } from '../response';
+import URLs from '../urls';
+
+const url = 'materials';
+const plural = 'materials';
+const single = 'material';
+
+const apiMaterials = new ApiCore(
+  {
+    getAll: true,
+    getSingle: true,
+    post: true,
+    put: false,
+    patch: true,
+    delete: false,
+    url: url,
+    plural: plural,
+    single: single,
+  },
+  true
+);
+
+const addMaterial = (type, data) => {
+  // return axios
+  //   .get(`${URLs.BASE_URL}/${}/${authorId}/books`, { params })
+  //   .then(handleResponse)
+  //   .catch(handleError);
+};
+
+apiMaterials.getAllTags = () => {
+  return axios
+    .get(`${URLs.BASE_URL}/${url}/tags`)
+    .then(handleResponse)
+    .catch(handleError);
+};
+
+// const apiMaterialsCommon = { addMaterial };
+
+export default apiMaterials;
