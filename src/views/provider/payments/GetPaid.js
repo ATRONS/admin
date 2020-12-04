d7975b48fae1fb8c721e713c177ad8ea953e5bc5
Henok Dejen <henokdejen84@gmail.com>
2020-12-04 16:21:04 +0300

    removed dummy datas and replaced them with data from the server

--- src/views/provider/payments/GetPaid.js
+++ src/views/provider/payments/GetPaid.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   Row,
   Card,
@@ -12,12 +12,21 @@ import {
   InputGroupAddon,
   Input,
   Button,
+  Spinner,
 } from 'reactstrap';
 
 import IntlMessages from '../../../helpers/IntlMessages';
 import { Colxx, Separator } from '../../../components/common/CustomBootstrap';
 import Breadcrumb from '../../../containers/navs/Breadcrumb';
 import classnames from 'classnames';
+import { CustomSpinner } from '../../../components/common/CustomSpinner';
+import { apiPaymnet } from '../../../services/api/provider-related/payment';
+
+const basicDataDummy = {
+  availableBalance: 450,
+  withdrawalFee: 4,
+  minimum: 100,
+};
 
 const countDecimals = (num) => {
   let splitted = num.toString().split('.');
@@ -25,9 +34,8 @@ const countDecimals = (num) => {
 };
 
 const GetPaid = ({ match }) => {
-  const availableBalance = 4560.36;
-  const fee = 6;
-  const minimumWithdrawableAmount = 100;
+  const [basicData, setBasicData] = useState(null);
+  const [basicDataLoading, setBasicDataLoading] = useState(true);
 
   const [amountError, setAmountError] = useState('');
   const [amount, setAmount] = useState('');
@@ -35,6 +43,24 @@ const GetPaid = ({ match }) => {
 
   const [submitting, setSubmitting] = useState(false);
   const [submitted, setSubmitted] = useState(false);
+
+  useEffect(() => {
+    //
+    // apiPaymnet
+    //   .paymentInitialData()
+    //   .then((res) => {
+    //     setBasicData(basicDataDummy);
+    //   })
+    //   .catch((res) => {})
+    //   .finally(() => {
+    //     setBasicDataLoading(false);
+    //   });
+
+    setTimeout(() => {
+      setBasicData(basicDataDummy);
+      setBasicDataLoading(false);
+    }, 1000);
+  }, []);
 
   const setError = (error) => {
     setAmountError(error);
@@ -45,12 +71,16 @@ const GetPaid = ({ match }) => {
     const val = Number(e.target.value);
     if (val) {
       console.log('Bechegnenet', countDecimals(val));
-      if (val > availableBalance) {
-        setError("Amount can't be greater than ETB " + availableBalance);
+      if (val > basicData.availableBalance) {
+        setError(
+          "Amount can't be greater than ETB " + basicData.availableBalance
+        );
       } else if (countDecimals(e.target.value) !== 2) {
         setError('Amount must be a numerical value with two decimal places');
-      } else if (val < minimumWithdrawableAmount) {
-        setError("Amount can't be less than ETB " + minimumWithdrawableAmount);
+      } else if (val < basicData.minimum) {
+        setError(
+          "Amount can't be less than ETB " + basicData.minimum.toFixed(2)
+        );
       } else {
         setError('');
       }
@@ -70,6 +100,113 @@ const GetPaid = ({ match }) => {
     }, 2000);
   };
 
+  let mainContent = <CustomSpinner />;
+
+  if (basicData) {
+    if (submitted) {
+      mainContent = (
+        <div className="text-center">
+          <i
+            className="simple-icon-check d-block mb-3 text-success"
+            style={{ fontSize: '3em' }}
+          />
+
+          <h1 className="mb-2 text-success">Success</h1>
+          <p>Your withdrawal request has been successfully submitted.</p>
+        </div>
+      );
+    } else {
+      mainContent = (
+        <Form onSubmit={onsubmit}>
+          <FormGroup>
+            <Label for="exCustomCheckbox">
+              <strong>
+                <IntlMessages id="dashbaord.available_amount" />
+              </strong>
+            </Label>
+            <div>
+              <span>ETB {basicData.availableBalance}</span>
+            </div>
+          </FormGroup>
+
+          <FormGroup>
+            <Label for="exCustomInline">
+              <strong>
+                <IntlMessages id="forms.amount" />
+              </strong>
+            </Label>
+            <InputGroup className="mb-3" style={{ width: 'fit-content' }}>
+              <InputGroupAddon addonType="prepend">ETB</InputGroupAddon>
+              <Input
+                placeholder={'0.00'}
+                onChange={onAmountChange}
+                value={amount}
+                className="text-right"
+              />
+            </InputGroup>
+            <div className="text-danger">{amountError}</div>
+          </FormGroup>
+
+          <Row>
+            <Colxx xss={12}>
+              Withdrawal Fee (per payment) <br />
+              Other bank fees may apply.
+            </Colxx>
+            <Colxx xss={12}>
+              <strong>ETB {basicData.withdrawalFee.toFixed(2)}</strong>
+            </Colxx>
+          </Row>
+
+          <Separator className="mt-3 mb-3" />
+
+          <Row className="mb-3">
+            <Colxx xss={12}>
+              <strong>Total Amount</strong>
+            </Colxx>
+            <Colxx xss={12}>
+              <h2>
+                <strong>
+                  ETB{' '}
+                  {amount > 0 && formValid
+                    ? (amount - basicData.withdrawalFee).toFixed(2)
+                    : '0.00'}
+                </strong>
+              </h2>
+            </Colxx>
+          </Row>
+          <Separator className="mt-3 mb-3" />
+
+          <div className="mb-3 mt-3">
+            You are about to send{' '}
+            <strong>
+              ETB{' '}
+              {amount > 0 && formValid
+                ? amount - basicData.withdrawalFee
+                : '0.00'}
+            </strong>{' '}
+            to your Hello Cash account
+          </div>
+
+          <Button
+            color="primary"
+            type="submit"
+            className={` mt-3 btn-multiple-state  ${classnames({
+              'show-spinner': submitting,
+            })}`}
+            disabled={!formValid}
+          >
+            <span className="spinner d-inline-block">
+              <span className="bounce1" />
+              <span className="bounce2" />
+              <span className="bounce3" />
+            </span>
+            <span className="label">Get Paid Now</span>
+          </Button>
+        </Form>
+      );
+    }
+  }
+
   return (
     <Row>
       <Colxx xxs="12">
@@ -84,98 +221,7 @@ const GetPaid = ({ match }) => {
               <IntlMessages id="payment.withdrawal_request" />
             </CardTitle>
 
-            {submitted ? (
-              <div className="text-center">
-                <i
-                  className="simple-icon-check d-block mb-3 text-success"
-                  style={{ fontSize: '3em' }}
-                />
-
-                <h1 className="mb-2 text-success">Success</h1>
-                <p>Your withdrawal request has been successfully submitted.</p>
-              </div>
-            ) : (
-              <Form onSubmit={onsubmit}>
-                <FormGroup>
-                  <Label for="exCustomCheckbox">
-                    <strong>
-                      <IntlMessages id="dashbaord.available_amount" />
-                    </strong>
-                  </Label>
-                  <div>
-                    <span>ETB 4560.34</span>
-                  </div>
-                </FormGroup>
-
-                <FormGroup>
-                  <Label for="exCustomInline">
-                    <strong>
-                      <IntlMessages id="forms.amount" />
-                    </strong>
-                  </Label>
-                  <InputGroup className="mb-3" style={{ width: 'fit-content' }}>
-                    <InputGroupAddon addonType="prepend">ETB</InputGroupAddon>
-                    <Input
-                      placeholder={'0.00'}
-                      onChange={onAmountChange}
-                      value={amount}
-                      className="text-right"
-                    />
-                  </InputGroup>
-                  <div className="text-danger">{amountError}</div>
-                </FormGroup>
-
-                <Row>
-                  <Colxx xss={12}>
-                    Withdrawal Fee (per payment) <br />
-                    Other bank fees may apply.
-                  </Colxx>
-                  <Colxx xss={12}>
-                    <strong>ETB {fee.toFixed(2)}</strong>
-                  </Colxx>
-                </Row>
-
-                <Separator className="mt-3 mb-3" />
-
-                <Row className="mb-3">
-                  <Colxx xss={12}>
-                    <strong>Total Amount</strong>
-                  </Colxx>
-                  <Colxx xss={12}>
-                    <h2>
-                      <strong>
-                        ETB {amount > 0 && formValid ? amount - fee : '0.00'}
-                      </strong>
-                    </h2>
-                  </Colxx>
-                </Row>
-                <Separator className="mt-3 mb-3" />
-
-                <div className="mb-3 mt-3">
-                  You are about to send{' '}
-                  <strong>
-                    ETB {amount > 0 && formValid ? amount - fee : '0.00'}
-                  </strong>{' '}
-                  to your Hello Cash account
-                </div>
-
-                <Button
-                  color="primary"
-                  type="submit"
-                  className={` mt-3 btn-multiple-state  ${classnames({
-                    'show-spinner': submitting,
-                  })}`}
-                  disabled={!formValid}
-                >
-                  <span className="spinner d-inline-block">
-                    <span className="bounce1" />
-                    <span className="bounce2" />
-                    <span className="bounce3" />
-                  </span>
-                  <span className="label">Get Paid Now</span>
-                </Button>
-              </Form>
-            )}
+            {mainContent}
           </CardBody>
         </Card>
       </Colxx>
