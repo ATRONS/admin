5e8537afac20ba9e6f6e042babd362104e82231f
Henok Dejen <henokdejen84@gmail.com>
2020-12-04 16:24:37 +0300

    removed unnecessary sorting options

--- src/containers/materials/MaterialListingHeading.js
+++ src/containers/materials/MaterialListingHeading.js
@@ -95,24 +95,6 @@ const MaterialListingHeading = ({
             </span>
 
             <div className="d-block d-md-inline-block pt-1">
-              <UncontrolledDropdown className="mr-1 float-md-left btn-group mb-1">
-                <DropdownToggle caret color="outline-dark" size="xs">
-                  <IntlMessages id="pages.orderby" />
-                  {selectedOrderOption.label}
-                </DropdownToggle>
-                <DropdownMenu>
-                  {orderOptions.map((order, index) => {
-                    return (
-                      <DropdownItem
-                        key={index}
-                        onClick={() => changeOrderBy(order.column)}
-                      >
-                        {order.label}
-                      </DropdownItem>
-                    );
-                  })}
-                </DropdownMenu>
-              </UncontrolledDropdown>
               <div className="search-sm d-inline-block float-md-left mr-1 mb-1 align-top">
                 <input
                   type="text"
